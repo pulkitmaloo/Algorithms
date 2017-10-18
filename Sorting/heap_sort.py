d15cfbe3e99b9f764d18ec041baf096a6386e9ce
Pulkit Maloo <maloop@iu.edu>
2017-10-17 23:52:24 -0400

    added files

--- Sorting/heap_sort.py
+++ Sorting/heap_sort.py
@@ -1,9 +1,23 @@
 # -*- coding: utf-8 -*-
-s = "nitin"
+def left(i):
+    return 2*i + 1
+def right(i):
+    return 2*i + 2
 
-def count(s):
-    count = 0
-    for l in range(len(s),0,-1):
-        for i in range(2):
+def max_heapify(arr, n, i):
+    l = left(i)
+    r = right(i)
+    largest = i
+    if l < len(arr) and arr[l] > arr[i]:
+        largest = l
+    if r < len(arr) and arr[r] > arr[largest]:
+        largest = r
+    if largest != i:
+        arr[i], arr[largest] = arr[largest], arr[i]
+        max_heapify(arr, n, largest)
 
-count(s)
+
+
+arr = [3,4,2]
+
+max_heapify(arr, len(arr), 0)
