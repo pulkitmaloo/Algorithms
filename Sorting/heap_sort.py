ea52e4dae12647818f296ffd4737ca694301e734
Pulkit Maloo <maloop@iu.edu>
2017-10-18 00:13:36 -0400

    added heap sort

--- Sorting/heap_sort.py
+++ Sorting/heap_sort.py
@@ -8,16 +8,27 @@ def max_heapify(arr, n, i):
     l = left(i)
     r = right(i)
     largest = i
-    if l < len(arr) and arr[l] > arr[i]:
+    if l < n and arr[l] > arr[i]:
         largest = l
-    if r < len(arr) and arr[r] > arr[largest]:
+    if r < n and arr[r] > arr[largest]:
         largest = r
     if largest != i:
         arr[i], arr[largest] = arr[largest], arr[i]
         max_heapify(arr, n, largest)
 
+def build_heap(arr, n):
+    for i in range(n/2-1, -1, -1):
+        max_heapify(arr, n, i)
 
+def heap_sort(arr, n):
+    build_heap(arr, n)
+    for i in range(n-1, 0, -1):
+        arr[0], arr[i] = arr[i], arr[0]
+        max_heapify(arr, i, 0)
 
-arr = [3,4,2]
+import random as rand
+arr = [rand.randint(0, 100) for i in range(20)]
+print arr
 
-max_heapify(arr, len(arr), 0)
+heap_sort(arr, len(arr))
+print arr
